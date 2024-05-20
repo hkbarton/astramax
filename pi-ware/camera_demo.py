8005aac311e897506da9289c3bfcaf0e29705299
hkbarton <hkbarton@gmail.com>
2024-05-19 18:33:02 -0700

    video poc

--- pi-ware/camera_demo.py
+++ pi-ware/camera_demo.py
@@ -1,10 +1,16 @@
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder, Quality
+from libcamera import Transform
 import time
 
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
+camera_config = picam2.create_video_configuration(
+  main={"size": (1920, 1080)},
+  transform=Transform(hflip=True, vflip=True)
+)
 picam2.configure(camera_config)
-picam2.start_preview(Preview.NULL)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+picam2.set_controls({"AfMode": 2 ,"AfTrigger": 0})
+encoder = H264Encoder()
+picam2.start_recording(encoder, 'test.h264', quality=Quality.VERY_HIGH)
+time.sleep(5)
+picam2.stop_recording()
